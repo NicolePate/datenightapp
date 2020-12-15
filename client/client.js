1e54241e7660ae72aaddf19d723acd92c30f2bba
NicolePate <nicole.pate512@gmail.com>
2020-12-15 13:29:53 -0600

    backend done

---
+++ client/client.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('http://localhost:3000/getAll')
+    .then(response => response.json())
+    .then(data => loadHTMLTable(data['data']));
+    
+});
+
+
+function loadHTMLTable(data) {
+    const table = document.querySelector('table tbody');
+
+    console.log(data);
+    
+    if (data.length === 0) {
+        table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data</td></tr>";
+        
+    }
+}
+
